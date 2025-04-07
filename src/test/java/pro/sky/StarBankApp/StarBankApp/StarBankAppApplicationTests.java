12ecc7802f750254c07ba557ec3c4365c068282e
Artem <artem.yaremenko.1992@gmail.com>
2025-04-07 19:56:23 +0300

    SpringBoot

---
+++ src/test/java/pro/sky/StarBankApp/StarBankApp/StarBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.StarBankApp.StarBankApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarBankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
