7e6bb1b4ca46dc22a08c0508f50555d93f0bcc47
Artem <artem.yaremenko.1992@gmail.com>
2025-05-22 23:12:49 +0300

    Добавил кэширование

--- src/main/java/pro/sky/StarBankApp/StarBankApp/repository/dinamicRules/RuleRepository.java
+++ src/main/java/pro/sky/StarBankApp/StarBankApp/repository/dinamicRules/RuleRepository.java
@@ -1,4 +1,4 @@
-package pro.sky.StarBankApp.StarBankApp.rules;
+package pro.sky.StarBankApp.StarBankApp.repository.dinamicRules;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.sky.StarBankApp.StarBankApp.model.rule.DynamicRule;
