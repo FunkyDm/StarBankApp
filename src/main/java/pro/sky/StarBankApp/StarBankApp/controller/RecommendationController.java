01ec7968c25f6bcd90da88c9f4aeb241c11d8e76
FunkyDm <mrforxbox@mail.ru>
2025-05-22 23:34:31 +0300

    Рефакторинг

--- src/main/java/pro/sky/StarBankApp/StarBankApp/controller/RecommendationController.java
+++ src/main/java/pro/sky/StarBankApp/StarBankApp/controller/RecommendationController.java
@@ -1,23 +1,26 @@
 package pro.sky.StarBankApp.StarBankApp.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.StarBankApp.StarBankApp.dto.Recommendation;
 import pro.sky.StarBankApp.StarBankApp.service.RecommendationService;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/recommendation")
+@RequiredArgsConstructor
 public class RecommendationController {
     private final RecommendationService recommendationService;
 
-    public RecommendationController(RecommendationService recommendationService){
-        this.recommendationService = recommendationService;
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<Recommendation>> getRecommendations(@PathVariable UUID userId) {
+        return ResponseEntity.ok(recommendationService.getRecommendations(userId));
     }
 
-    @GetMapping("/recommendation/{user_id}")
-    public Recommendation getRecommendations(@PathVariable("user_id")UUID userId){
-        return recommendationService.getRecommendations(userId.toString());
-    }
 }
