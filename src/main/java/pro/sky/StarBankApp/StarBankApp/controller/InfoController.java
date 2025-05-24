d1e4928c75314b2339d5f4d9073145f65594b12c
FunkyDm <mrforxbox@mail.ru>
2025-05-24 22:25:32 +0300

    Рефакторинг

--- src/main/java/pro/sky/StarBankApp/StarBankApp/controller/InfoController.java
+++ src/main/java/pro/sky/StarBankApp/StarBankApp/controller/InfoController.java
@@ -4,10 +4,11 @@ import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/management")
 public class InfoController {
 
